a7d11d904df90c6633a7c892dd6ef3afaaf4bf5a
Atharv <atharvkale22@gmail.com>
2024-02-02 19:31:02 +0530

    Start

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Student Mark Entry System"
+include(":app")
+ 
